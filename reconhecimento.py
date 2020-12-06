5325412213681eaafd7cfc6e44d53235d1c85421
AlexCouto <64182631+AlexCouto@users.noreply.github.com>
2020-12-06 20:54:09 -0300

    limpada no código e aprimiração do modelo

--- reconhecimento.py
+++ reconhecimento.py
@@ -1,5 +1,5 @@
 import numpy as np
-import tensorflow as tf
+import tensorflow
 from tensorflow import keras
 import cv2
 
@@ -7,14 +7,11 @@ model = keras.models.load_model("model")
 
 def classify_image(image_path):
     image = cv2.imread(image_path)
-    output = image.copy()
-    image = cv2.resize(image,(32,32)).flatten()
+    image = cv2.resize(image,(64,64)).flatten()
 
     classification = {}
 
     predictions = model.predict(np.array([image]))[0]
-
-    print(predictions)
 
     if(predictions[1] > predictions[0]):
         classification["status"] = "damaged"
@@ -25,8 +22,13 @@ def classify_image(image_path):
 
     return classification
 
+print("not damaged : ")
+for i in range(52):
+    print("i = ",i+1,classify_image("roofs/not_damaged/"+str(i+1)+".png"))
 
-print(classify_image("roofs/damaged/18.png"))
+print("damaged : ")
+for i in range(53):
+    print("i = ",i+1,classify_image("roofs/damaged/"+str(i+1)+".png"))
 
 
 
--- treinamento_modelo.py
+++ treinamento_modelo.py
@@ -7,63 +7,56 @@ from keras.utils import to_categorical
 import numpy as np
 from cv2 import cv2
 import os
-import random
+from random import shuffle
 from sklearn.model_selection import train_test_split
 
+# carregar imagens para o treinamento
+def load_images(folder , lbl):
+    for filename in os.listdir(img_folder):
+        image = cv2.imread(os.path.join(img_folder,filename))
+        if image is not None:
+            image = cv2.resize(image,(64,64)).flatten()
+            images.append(image)
+            labels.append(lbl)
 
-img_folder = "roofs/damaged"
 images = []
 labels = []
 
-for filename in os.listdir(img_folder):
-    image = cv2.imread(os.path.join(img_folder,filename))
-    if image is not None:
-        image = cv2.resize(image,(32,32)).flatten()
-        images.append(image)
-        labels.append(1)
+img_folder = os.path.join("roofs","damaged")
+load_images(img_folder,1)
 
+img_folder = os.path.join("roofs","not_damaged")
+load_images(img_folder,0)
 
-img_folder = "roofs/not_damaged"
-
-for filename in os.listdir(img_folder):
-    image = cv2.imread(os.path.join(img_folder,filename))
-    if image is not None:
-        image = cv2.resize(image,(32,32)).flatten()
-        images.append(image)
-        labels.append(0)
-
-
+# randomizar os arrays de imagens e labels da mesma forma
 c = list(zip(images, labels))
-
-random.shuffle(c)
-
+shuffle(c)
 images, labels = zip(*c)
 
+# converter os objetos em numpy.ndarray ( valores RGB das imagens é dividido por 255 para obtermos floats de 0 a 1)
 images = np.array(images, dtype = "float") / 255.0
 labels = np.array(labels)
 
-(x_train , x_test , y_train , y_test) = train_test_split(images, labels, test_size =  0.15 , random_state = 42)
-
-
+# separar amostras de teste e de treinamento
+(x_train , x_test , y_train , y_test) = train_test_split(images, labels, test_size =  0.25 , random_state = 42)
+# converter os labels em matrizes binárias
 y_train_one_hot = to_categorical(y_train)
 y_test_one_hot = to_categorical(y_test)
 
-#model
-
+# modelo simples de rede neural
 model = Sequential()
-model.add(Dense(1024, input_shape=(3072,), activation="sigmoid"))
-model.add(Dense(512, activation="sigmoid"))
+model.add(Dense(120, input_shape=(12288,), activation="sigmoid"))
+model.add(Dense(60, activation="sigmoid"))
 model.add(Dense(2, activation="softmax"))
 
-###
-INIT_LR = 0.01
-EPOCHS = 80
-
+# compilar o modelo
+INIT_LR = 0.05
+EPOCHS = 140
 opt = SGD(lr=INIT_LR)
-model.compile(loss="categorical_crossentropy", optimizer=opt,metrics=["accuracy"])
-
-
+model.compile(loss="binary_crossentropy", optimizer=opt , metrics=["accuracy"])
+# treinar a rede neural
 H = model.fit(x=x_train, y=y_train_one_hot, validation_data=(x_test, y_test_one_hot),epochs=EPOCHS, batch_size=32)
 
-print("[INFO] serializing network and label binarizer...")
+#salvar o modelo na pasta /model do projeto
+print("Saving model...")
 model.save("model")
