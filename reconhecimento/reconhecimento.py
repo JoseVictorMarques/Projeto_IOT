51817bc69c8153b31a3f2d89b337e08f2752247b
AlexCouto <64182631+AlexCouto@users.noreply.github.com>
2020-12-07 23:20:20 -0300

    Tudo numa pasta

---
+++ reconhecimento/reconhecimento.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow
+from tensorflow import keras
+import cv2
+
+model = keras.models.load_model("model")
+
+def classify_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.resize(image,(64,64)).flatten()
+
+    classification = {}
+
+    predictions = model.predict(np.array([image]))[0]
+
+    if(predictions[1] > predictions[0]):
+        classification["status"] = "damaged"
+        classification["probability"] = predictions[1]
+    else:
+        classification["status"] = "not_damaged"
+        classification["probability"] = predictions[0]
+
+    return classification
+
+print(classify_image("roofs/damaged/6.png"))
+
+
+
